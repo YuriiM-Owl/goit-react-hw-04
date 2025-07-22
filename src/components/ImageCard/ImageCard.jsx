ae6d9f7420ee9f28b54599825413a0cc7d118071
YuriiM-Owl <129115158+YuriiM-Owl@users.noreply.github.com>
2025-07-22 05:05:48 +0300

    fix hw04

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,11 +2,12 @@ import s from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => {
   return (
-    <div className={s.card} onClick={() => onClick(image)}>
+    <div className={s.card}>
       <img
-        src={image.urls.thumb}
+        src={image.urls.small}
         alt={image.alt_description}
         className={s.image}
+        onClick={() => onClick(image)}
       />
     </div>
   );
